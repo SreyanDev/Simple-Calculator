68ad88c1ce1bbcab6a799b78db58912048c56651
Sreyan <sreyanmithra@gmail.com>
2024-10-12 12:32:54 +0530

    Adding try and except.
    Adding try and except keywords or function that is used to run the code even when it throws the error.
    
    1) For example, if I give string value instead of float or integer then it prints Enter the integer or float value.
    
    2) If I give float or integer value to begin with then the code execute perfectly.

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -3,45 +3,53 @@
 title = "Simple Calculator"
 print(title.center(150))
 
+
+
+try:                #Try this code, if the input is either float or integer (True) it runs or it will skip and go into except part.
+
 #This is used to initialize the input through the variable.
 
-a=float(input("Enter the first number: "))
-b=float(input("Enter the second number: "))
+    a=float(input("Enter the first number: "))
+    b=float(input("Enter the second number: "))
 
 #Function add with two parameters.
 
-def add(a,b):
-    x=a+b
-    print(f"\nThe sum of two numbers is: {x}")    #This f"{}" is a formatting string and \n to give the gap.
+    def add(a,b):
+        x=a+b
+        print(f"\nThe sum of two numbers is: {x}")    #This f"{}" is a formatting string and \n to give the gap.
 
 #Function difference with two parameters.
 
-def sub(a,b):
-    x=a-b
-    print(f"\nThe difference of two numbers is: {x}")
+    def sub(a,b):
+        x=a-b
+        print(f"\nThe difference of two numbers is: {x}")
 
 #Function multiply with two parameters.
 
-def mul(a,b):
-    x=a*b
-    print(f"\nThe multiplication of two numbers is: {x}")
+    def mul(a,b):
+        x=a*b
+        print(f"\nThe multiplication of two numbers is: {x}")
 
 #Function divide with two parameters.
 
-def div(a,b):
-    x=a/b
-    print(f"\nThe division of two numbers is: {x:.2f}")     #This {x.2f} is used to filer out the decimal points after two numbers.
+    def div(a,b):
+        x=a/b
+        print(f"\nThe division of two numbers is: {x:.2f}")     #This {x.2f} is used to filer out the decimal points after two numbers.
 
 #Function mean with two parameters.
 
-def mean(a,b):
-    x=(a+b)/2
-    print(f"\nThe mean of two numbers is: {x}")
+    def mean(a,b):
+        x=(a+b)/2
+        print(f"\nThe mean of two numbers is: {x}")
 
 #calling the functions.
 
-add(a,b)
-sub(a,b)
-mul(a,b)
-div(a,b)
-mean(a,b)
+    add(a,b)
+    sub(a,b)
+    mul(a,b)
+    div(a,b)
+    mean(a,b)
+
+except Exception as e:                  #If the try part gets error then print the exception printed below.
+
+    print(f"{e}\n\nEnter either float or integer number.")           #\n\n skips a line.
